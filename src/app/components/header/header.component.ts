3c5c21afd48684535b8269bbdb7a514d6c2739d6
xocomil <xocomil@gmail.com>
2022-08-27 17:03:41 -0600

    refactor to one observable

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { BehaviorSubject, Subject, tap } from 'rxjs';
+import {BehaviorSubject, finalize, forkJoin, map, merge, of, shareReplay, Subject, switchMap, tap} from 'rxjs';
 import { Product } from 'src/Interfaces/main';
 import { ProductsService } from 'src/services/products.service';
 
@@ -10,8 +10,26 @@ import { ProductsService } from 'src/services/products.service';
 })
 export class HeaderComponent implements OnInit {
   constructor(private productsService: ProductsService) {}
-  APICategories$ = new BehaviorSubject<string[]>(['All Categories']);
-  products$ = new Subject<Product[]>();
+
+  #categoriesFromServer$ = this.productsService.getCategories();
+  #defaultCategories$ = of(['All Categories']);
+
+  APICategories$ = forkJoin([this.#defaultCategories$, this.#categoriesFromServer$])
+    .pipe(
+      tap(() => {
+        console.log(`subscribed at ${new Date()}`);
+      }),
+      map(([defaultCategories, categoriesFromServer]) => defaultCategories.concat(categoriesFromServer)),
+      finalize(() => {
+        console.log(`Observable completed at ${new Date()}`)
+      }),
+      shareReplay()
+  );
+
+  // filters$ = new BehaviorSubject(1);
+  // products$ = this.filters$.pipe(switchMap(filters => this.productsService.getGenericProducts(filters)));
+
+    products$ = this.productsService.getGenericProducts();
 
   //The idea is to have a Behavior Subject with "All Categories" and to have the api call to get the rest of the categories.
   //Add on top of that observable so we have a select that has all the values. The All categories, and the other values that we get thru the api call.
@@ -23,13 +41,6 @@ export class HeaderComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.productsService.getGenericProducts().subscribe((data) => {
-      this.products$.next(data);
-    });
-
-    this.productsService.getCategories().subscribe((data) => {
-      this.APICategories$.next(data);
-    });
   }
 }
 
